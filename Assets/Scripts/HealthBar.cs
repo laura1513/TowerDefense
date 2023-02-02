535a3b1ebb765f54d6e1fa8323a5fc45280a98eb
laura1513 <laurajorques2003@gmail.com>
2023-02-02 20:32:15 +0100

    cositas

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class HealthBar : MonoBehaviour
 {
+    TextMesh tm;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tm = GetComponent<TextMesh>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.forward = Camera.main.transform.forward;
+    }
+
+    public int current()
+    {
+        return tm.text.Length;
     }
 }
